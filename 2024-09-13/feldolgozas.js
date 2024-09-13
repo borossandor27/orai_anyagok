2b36cb3d3a00a40bb603b0d1ece05ef71593f001
borossandor27 <boros.sanyi@gmail.com>
2024-09-13 13:13:25 +0200

    szamok osszeadasa

--- 2024-09-13/feldolgozas.js
+++ 2024-09-13/feldolgozas.js
@@ -1,6 +1,14 @@
 const gomb = document.getElementById("gomb");
 gomb.addEventListener("click", () => {
     // Az adatok feldolgozása
-    const nev = document.getElementById("nev").value;  
-    alert("Üdvözöllek, " + nev + "!");  
+    if (document.getElementById("szam1").value === "" || document.getElementById("szam2").value === "") {
+        alert("Kérlek add meg az összes adatot!");
+        return;
+    }
+    let szam1 = parseInt(document.getElementById("szam1").value);
+    let szam2 = parseInt(document.getElementById("szam2").value);
+    let eredmeny = "A szam1 és szam2 összege: " +(szam1 + szam2);
+
+    // Az eredmény kiírása
+    document.getElementById("eredmeny").innerText = eredmeny;
 });
