f2f5f4b84e8ef02daea2d840ceeef9bf1e69f157
Boros Sandor <55062355+borossandor27@users.noreply.github.com>
2025-02-02 16:36:15 +0100

    4.3.22

--- 2025-01-31B/src/App.jsx
+++ 2025-01-31B/src/App.jsx
@@ -1,87 +1,8 @@
 import UserCard from './components/UserCard'
+import users from './data/users.json'
 import './App.css'
 
 function App() {
-  const users = [
-    {
-      "profilkep": "https://avatar.iran.liara.run/public/33",
-      "nev": "Kiss József",
-      "email": "kissjosef@liara",
-      "telefonszam": "06201234567",
-      "statusz": "Aktív"
-    },
-    {
-      "profilkep": "https://avatar.iran.liara.run/public/8",
-      "nev": "Kovács Péter",
-      "email": "kovacs@liara",
-      "telefonszam": "06221177493",
-      "statusz": "Aktív"
-    },
-    {
-      "profilkep": "https://avatar.iran.liara.run/public/51",
-      "nev": "Nagy Anna",
-      "email": "nagy@liara",
-      "telefonszam": "06218255150",
-      "statusz": "Inaktív"
-    },
-    {
-      "profilkep": "https://avatar.iran.liara.run/public/94",
-      "nev": "Tóth Gábor",
-      "email": "toth@liara",
-      "telefonszam": "06256902061",
-      "statusz": "Inaktív"
-    },
-    {
-      "profilkep": "https://avatar.iran.liara.run/public/39",
-      "nev": "Szabó Katalin",
-      "email": "szabo@liara",
-      "telefonszam": "06247616277",
-      "statusz": "Inaktív"
-    },
-    {
-      "profilkep": "https://avatar.iran.liara.run/public/54",
-      "nev": "Varga László",
-      "email": "varga@liara",
-      "telefonszam": "06279980436",
-      "statusz": "Inaktív"
-    },
-    {
-      "profilkep": "https://avatar.iran.liara.run/public/25",
-      "nev": "Horváth Zoltán",
-      "email": "horvath@liara",
-      "telefonszam": "06237963866",
-      "statusz": "Inaktív"
-    },
-    {
-      "profilkep": "https://avatar.iran.liara.run/public/36",
-      "nev": "Balogh Éva",
-      "email": "balogh@liara",
-      "telefonszam": "06212643749",
-      "statusz": "Aktív"
-    },
-    {
-      "profilkep": "https://avatar.iran.liara.run/public/95",
-      "nev": "Farkas Dániel",
-      "email": "farkas@liara",
-      "telefonszam": "06277391705",
-      "statusz": "Inaktív"
-    },
-    {
-      "profilkep": "https://avatar.iran.liara.run/public/21",
-      "nev": "Kiss Anikó",
-      "email": "kiss@liara",
-      "telefonszam": "06308617931",
-      "statusz": "Aktív"
-    },
-    {
-      "profilkep": "https://avatar.iran.liara.run/public/95",
-      "nev": "Molnár Tamás",
-      "email": "molnar@liara",
-      "telefonszam": "06302199680",
-      "statusz": "Inaktív"
-    }
-  ]
-
   return (
     <>
       <div>
