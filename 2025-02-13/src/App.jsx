a76391feb16d6dc79e69a0099a410f7480d260e9
Boros Sandor <55062355+borossandor27@users.noreply.github.com>
2025-02-14 10:04:20 +0100

    4.52.2

--- 2025-02-13/src/App.jsx
+++ 2025-02-13/src/App.jsx
@@ -6,26 +6,48 @@ const baseURL = 'https://retoolapi.dev/MVwfIW/data'
 
 
 function App() {
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]) // kezdeti üres tömb
+
   useEffect(() => {
-    fetchUsers()
-  }, [])
-  const fetchUsers = async () => {
-    const { data } = await axios.get(baseURL)
-    console.log(data)
+    getAllUsers()
+  }, []) // csak mountoláskor fusson le
+  const getAllUsers = async () => {
+    const { data } = await axios.get(baseURL) // adatok lekérése
     setUsers(data)
   }
 
+  const createUser = async () => {
+    const nev = document.getElementById('nev').value
+    const fizetes = document.getElementById('fizetes').value
+    const { data } = await axios.post(baseURL, { nev, fizetes })
+    setUsers([...users, data])
+  }
+  const updateUser = async () => {
+    const id = document.getElementById('id').value
+    const nev = document.getElementById('nev').value
+    const fizetes = document.getElementById('fizetes').value
+    const { data } = await axios.put(`${baseURL}/${id}`, { nev, fizetes })
+    setUsers(users.map(user => user.id === id ? data : user))
+  }
+
+  const fillForm = (id) => {
+    const user = users.find(user => user.id === id)
+    document.getElementById('id').value = user.id
+    document.getElementById('nev').value = user.nev
+    document.getElementById('fizetes').value = user.fizetes
+  }
 
   return (
     <>
       <form>
         <fieldset>
           <legend>Kiválasztott felhasználó adatai</legend>
+          <input type="hidden" name="id" id="id" />
           <input type="text" name='nev' id='nev' placeholder='Felhasználó neve' />
           <input type="number" name="fizetes" id="fizetes" placeholder='fizetés' />
         </fieldset>
-        <MyButton color='green'>Küldés</MyButton>
+        <MyButton color='green' onClick={createUser}>Küldés</MyButton>
+        <MyButton color='red'>Módosítás</MyButton>
       </form>
       <table>
         <thead>
@@ -41,6 +63,8 @@ function App() {
             <tr key={id}>
               <td>{nev}</td>
               <td>{fizetes}</td>
+              <td><MyButton color='blue' onClick={() => fillForm(id)}>Módosít</MyButton></td>
+              <td><MyButton color='red'>Törlés</MyButton></td>
             </tr>
           ))}
         </tbody>
--- 2025-02-13/src/componens/MyButton.jsx
+++ 2025-02-13/src/componens/MyButton.jsx
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
 
 const MyButton = styled.button`
-    background-color: {props => props.color};
+    background-color: ${props => props.color};
     color: white;
     border-radius: 5px;
-    padding: 10px 20px;
-    font-size: 1.3rem;
+    padding: 5px 10px;
+    margin: 10px;
+    font-size: .8rem;
     cursor: pointer;
 `;
 
