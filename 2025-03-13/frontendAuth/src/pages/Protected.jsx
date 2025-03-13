cfaaf08eaea59bc518bdab866732f050a56f8a19
Boros Sandor <55062355+borossandor27@users.noreply.github.com>
2025-03-13 14:56:55 +0100

    4.63.3

--- 2025-03-13/frontendAuth/src/pages/Protected.jsx
+++ 2025-03-13/frontendAuth/src/pages/Protected.jsx
@@ -1,7 +1,14 @@
 const Protected = () => {
+
+    const handleLogout = () => {
+        localStorage.removeItem('token')
+        window.location.reload()
+    }
+    
     return (
         <>
             <h1>Protected</h1>
+            <button onClick={handleLogout}>Logout</button>
         </>
     )
 }
